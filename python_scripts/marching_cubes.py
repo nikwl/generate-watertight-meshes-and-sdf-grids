c8da23d2adafb3f69976fc72194d11c814426175
nikwl <nikolas.lamb@gmail.com>
2022-10-11 07:54:47 -0400

    using mcubes instead

--- python_scripts/marching_cubes.py
+++ python_scripts/marching_cubes.py
@@ -1,14 +1,17 @@
-import numpy as np
-import Vox
-from skimage import measure
-from plyfile import (PlyData, PlyElement, make2d, PlyParseError, PlyProperty)
 import argparse
+
+import numpy as np
+import mcubes
+from plyfile import (PlyData, PlyElement, make2d, PlyParseError, PlyProperty)
+
+import Vox
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--in_vox', type=str, required=True, help="filename of input vox file")
 parser.add_argument('--out_ply', type=str, required=True, help="filename of output ply file")
 parser.add_argument('--iso', type=float, default=0.0, help="iso surface value")
+parser.add_argument('--smooth', action="store_tru", default=False, help="do marching cubes smoothing?")
 
 
 def save_as_ply(filename, verts, faces, color=None):
@@ -26,17 +29,22 @@ if __name__ == '__main__':
     opt = parser.parse_args()
 
     grid = Vox.load_vox(opt.in_vox)
-    verts, faces, normals, _ = measure.marching_cubes(grid.sdf, opt.iso)
-    verts = np.fliplr(verts) # <-- go from zyx to xyz
+
+    surface_level = 0.0
+    sdf = grid.sdf
+    if opt.iso != 0.0:
+        sdf -= opt.iso
+    if opt.smooth:
+        sdf = mcubes.smooth(sdf)
+        surface_level = 0.5
+    vertices, faces = mcubes.marching_cubes(sdf, surface_level)
+    
+    verts = np.fliplr(vertices) # <-- go from zyx to xyz
 
     rot = grid.grid2world[0:3,0:3]
     trans = grid.grid2world[0:3,3]
     verts = np.matmul(verts, rot.transpose()) + trans
 
-
     color = [200, 50, 50]
 
     save_as_ply(opt.out_ply, verts, faces, color)
-
-
-
